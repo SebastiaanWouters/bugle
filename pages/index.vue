46c0e6b25ff5890a062ce49b7205563930a686be
SebastiaanWouters <sebastiaan.wouters@kuleuven.be>
2024-03-19 23:48:16 +0100

    feat: add websocket server / client

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+onMounted(() => { const ws = new WebSocket('ws://localhost:3000/_ws'); setTimeout(() => { ws.send('test')}, 1000) });
+</script>
+
+
+<template>
+  <div>
+    <h1>WS DEMO</h1>
+  </div>
+</template>
